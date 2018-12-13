2d64a6db09ed6a3bcf41cb2bb7d70bd5f1120a6b
Yen <iknepper@hotmail.com>
2018-12-13 02:11:02 -0500

    Mute button added

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -6,6 +6,7 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+const muteButton = player.querySelector('.mute__button')
 
 /* Build out functions */
 function togglePlay() {
@@ -15,10 +16,30 @@ function togglePlay() {
 
 function updateButton(){
   const icon = this.paused ? '►' : '❚ ❚';
-  console.log('Update button!')
+  // console.log('Update button!')
   toggle.textContent = icon
+}
+//skip
+function skip(){
+  video.currentTime += parseFloat(this.dataset.skip)
+}
+//sliders
+let lastVol = video.volume
 
 
+function handleRangeUpdate(){
+  console.log(this)
+  video[this.name] =  this.value
+  if(this.name === 'volume') lastVol = video.volume
+}
+//mute
+function muteVolume(){
+  if(video.volume > 0) video.volume = 0
+  else video.volume =lastVol
+
+
+  console.log(video.volume)
+  // video.
 }
 /*  hook up event listeners*/
 
@@ -27,3 +48,11 @@ video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 
 toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip))
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate))
+
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate))
+
+muteButton.addEventListener('click', muteVolume)
